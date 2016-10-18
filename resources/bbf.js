3b0dd084c0214dfc9d5d58c8848191526b9d63b4
bringbackfrankie <bringbackfrankie@gmail.com>
2016-10-18 13:17:05 +1100

    Update bbf.js

--- resources/bbf.js
+++ resources/bbf.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $("#clickButton").click(function(event){
      $("#steps").slideToggle("fast","swing");
+     
+      if($(this).attr("class") == "active")
+        $(this).removeClass("active");
+      else
+        $(this).addClass("active");
   });
 });
